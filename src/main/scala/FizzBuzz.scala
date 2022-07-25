bd04a260d44b3c337474125df4d9db4da370a80a
Ed <ed321@hotmail.co.uk>
2022-07-25 14:35:27 +0100

    Adds FizzBuzz to the list when divisible by five and three

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -2,7 +2,9 @@ object FizzBuzz{
   def generate(number: Int): String = {
     val resultArr = new Array[String](number)
     for( current <- 1 to number){
-      if(current % 3 == 0){
+      if(current % 3 == 0 && current % 5 == 0) {
+        resultArr(current-1) = "FizzBuzz"
+      } else if(current % 3 == 0){
         resultArr(current-1) = "Fizz"
       } else if(current % 5 == 0){
         resultArr(current-1) = "Buzz"
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -7,10 +7,13 @@ class FizzBuzzTest extends AnyFunSuite {
   test("lists the numbers up to two") {
     assert(FizzBuzz.generate(2) === "1, 2")
   }
-  test("lists Fizz divisible by three") {
+  test("replaces Fizz divisible by three") {
     assert(FizzBuzz.generate(3) === "1, 2, Fizz")
   }
-  test("lists Buzz when divisible by five"){
+  test("replaces Buzz when divisible by five"){
     assert(FizzBuzz.generate(5) === "1, 2, Fizz, 4, Buzz")
+  }
+  test("replaces FizzBuzz when divisible by five and three") {
+    assert(FizzBuzz.generate(15) === "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz")
   }
 }
