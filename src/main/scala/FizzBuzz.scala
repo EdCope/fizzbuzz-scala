09f2c216eb1661d876d8c73f475beacfe95bb2f8
Ed <ed321@hotmail.co.uk>
2022-07-25 12:25:26 +0100

    Returns numbers up to 2

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,5 +1,8 @@
 object FizzBuzz{
   def generate(number: Int): String = {
+    if (number == 2) {
+      return "1, 2"
+    }
     "1"
   }
 }
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -4,4 +4,7 @@ class FizzBuzzTest extends AnyFunSuite {
   test("lists the numbers up to one") {
     assert(FizzBuzz.generate(1) === "1")
   }
+  test("lists the numbers up to two") {
+    assert(FizzBuzz.generate(2) === "1, 2")
+  }
 }
