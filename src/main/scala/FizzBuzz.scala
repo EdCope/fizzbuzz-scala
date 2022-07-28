766026b82d19503e006a57f7a3be27e8759ccbf9
Ed <ed321@hotmail.co.uk>
2022-07-28 14:33:51 +0100

    Remove dirs

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,21 +1,5 @@
 object FizzBuzz{
   def generate(number: Int): String = {
-    //imperative solution
-//    val resultArr = new Array[String](number)
-//    for( current <- 1 to number){
-//      if(current % 3 == 0 && current % 5 == 0) {
-//        resultArr(current-1) = "FizzBuzz"
-//      } else if(current % 3 == 0){
-//        resultArr(current-1) = "Fizz"
-//      } else if(current % 5 == 0){
-//        resultArr(current-1) = "Buzz"
-//      } else {
-//        resultArr(current-1) = s"$current"
-//      }
-//    }
-//    resultArr.filter(_ != null).mkString(", ")
-    //functional solution
-//    val arr = Array.range(1, number).map(_.toString)
     (1 to number).map(replaceNumber(_)).mkString(", ")
   }
   def divisibleBy(divisor: Int, current: Int) = current % divisor == 0
